906eac73072d9a5757edb47b6d7a110607430b2d
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-17 17:18:51 +0000

    {

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
