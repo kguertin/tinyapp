f75e9075343c16a6dd7efc3f15df9933b0e41285
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-17 16:46:11 +0000

    initial server setup using express

---
+++ express_server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
