db54179bdb167f9a72f36db3df57b5a8a2c60691
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-02-17 18:34:49 +0000

    adding template update and dynamic generation of short and long urls

--- express_server.js
+++ express_server.js
@@ -18,6 +18,11 @@ app.get('/urls', (req, res) => {
   res.render('urls_index',templateVars);
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render('urls_show', templateVars);
+});
+
 app.get('/hello', (req, res) => {
   let templateVars = { greeting: 'Hello World!'};
   res.render('hello_world', templateVars);
